b30e8e1979f47988bab44bcdb47ecfd7f3419aa2
Axel <BBRenHao35@gmail.com>
2022-09-18 11:39:24 +0800

    Update DataGuides.py
    Update DataGuides.py

--- DataGuides.py
+++ DataGuides.py
@@ -1,8 +1,11 @@
 ##### data_guides (去除重複路徑)
 
+
+```python
 def data_guides(list_xml): # 傳入list_xml(未處理的所有標籤(深度+標籤))
     list_data_guides = []
     for x in list_xml:
         if x not in list_data_guides :
             list_data_guides.append(x)
     return list_data_guides # 回傳不重複的深度+標籤
+```
